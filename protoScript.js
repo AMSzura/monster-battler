f1e1916355e3ec77d684654adec243dcd4460003
Aaron Szura <aaron.szura@gmail.com>
2022-06-01 16:31:41 +0100

    create basic monster object and attack function

---
+++ protoScript.js
@@ -0,0 +1,33 @@
+
+
+
+const monster = {
+    totalHealth: 100,
+    currentHealth: 100,
+    attackValue: 20,
+    defenseValue: 8,
+    attack(target) {
+        damageValue = (this.attackValue/2);
+        target.currentHealth = target.currentHealth - damageValue;
+        console.log(this.name + " attacked " + target.name + " for " + damageValue + " damage!");
+    }
+}
+
+
+const player = Object.create(monster);
+player.name = "squirtle";
+
+const enemy = Object.create(monster);
+enemy.name = "charmander";
+
+
+player.attack(enemy);
+
+console.log(enemy);
+
+player.attack(enemy);
+
+console.log(enemy);
+
+enemy.attack(player);
+console.log(player);
