7cb3628f30860cf8711377ed5fb8ff90d3a6eb5e
Aaron Szura <aaron.szura@gmail.com>
2022-06-08 14:41:26 +0100

    create more robust attack fnct, turn checker

--- protoScript.js
+++ protoScript.js
@@ -16,21 +16,60 @@ class Cpu {
 
 // class for constructing individual monster objects
 class Monster {
-    constructor(name, totalHealth, element, attackVal, defenseVal) {
+    constructor(name, totalHealth, element, attackVal, defenseVal, moves) {
         this.name = name;
         this.totalHealth = totalHealth;
         this.element = element;
         this.attackVal = attackVal;
         this.defenseVal = defenseVal;
+        this.moves = moves;
     }
 }
 
-const squirtle = ["Squirtle", 100, "water", 14, 20];
+let target;
+let attacker;
+let playerTurn = true;
+
+function determineTarget() {
+    if (playerTurn) {
+        attacker = player.lineUp[0];
+        target = cpu.lineUp[0];
+    } else if (!playerTurn) {
+        attacker = cpu.lineUp[0];
+        target = player.lineUp[0];
+    }
+    console.log("attacker is " + attacker + ". target is " + target);
+}
+
+function endTurn() {
+    (playerTurn) ? playerTurn = false : playerTurn = true;
+}
+
+function scratch() {
+    determineTarget();
+    damage = attacker.attackVal/2;
+    defense = target.defenseVal/2;
+    target.currentHealth =- damage;
+    console.log(attacker.name + " hit " + target.name + " for " + damage + " damage!");
+    endTurn();
+    }
 
 
-let Squirtle = new Monster (...squirtle);
+const squirtleData = ["Squirtle", 100, "water", 14, 20, [scratch]];
+const rocklerData = ["Rockler", 100, "rock", 12, 22, [scratch]];
 
-console.log(Squirtle);
+
+let squirtle = new Monster (...squirtleData);
+let rockler = new Monster (...rocklerData);
+
+const player = new Player ("aaron", [squirtle]);
+const cpu = new Cpu ("bob", [rockler]);
+
+console.log(squirtle);
+console.log(rockler);
+
+console.log(player);
+console.log(cpu);
 
 
 
